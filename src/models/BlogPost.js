e8bdf425c1f1b42e9dd69f9766ef699adb488337
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-05-12 18:16:58 -0300

    Requisito 10

---
+++ src/models/BlogPost.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define(
+    'BlogPost',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, },
+      title: { type: DataTypes.STRING, allowNull: false, },
+      content: { type: DataTypes.STRING, allowNull: false, },
+      userId: { type: DataTypes.INTEGER, field: 'user_id' },
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'blog_posts',
+    },
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { foreignKey: 'user_id', as: 'users' });
+  };
+
+  return BlogPost;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -14,5 +14,10 @@ module.exports = (sequelize, DataTypes) => {
     tableName: 'users',
   },
 );
+
+User.associate = (models) => {
+  User.hasMany(models.BlogPost, { foreignKey: 'user_id', as: 'blog_posts' });
+};
+
   return User;
 };
